e493fe32de84730f22b00108a836d5b51a47ed80
nengkya <nengkya@yahoo.com>
2024-12-19 06:22:25 +0700

    rsi = register source index

--- hello.s
+++ hello.s
@@ -1,17 +1,17 @@
 .data
-hello:
-    .ascii "Hello\n\0"
+wah:
+    .ascii "Wah !\n\0"
 
 .text
-.globl _start #.global valid too
+.globl _start 	   #.global valid too
 
 _start:	
-    mov $1, %rax #syscall: sys_write, rax = Register Accumulator eXtended
-    mov $1, %rdi #stdout, rdi = register destination index
-    mov $hello, %rsi #char * address
-    mov $10, %rdx #string length, rdx = Register Data eXtended
-    syscall #calls the kernel
+    mov $1, %rax   #syscall: sys_write, rax = Register Accumulator eXtended
+    mov $1, %rdi   #stdout, rdi = register destination index
+    mov $wah, %rsi #char * address, rsi = register source index
+    mov $10, %rdx  #string length, rdx = Register Data eXtended
+    syscall 	   #calls the kernel
 
-    mov $60, %rax #syscall: sys_exit
+    mov $60, %rax  #syscall: sys_exit
     xor %rdi, %rdi #exit status: 0
     syscall
