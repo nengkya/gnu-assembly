53c5c57881ffe8be84f834713f5a41b9145f4ebd
nengkya <nengkya@yahoo.com>
2024-12-19 05:20:23 +0700

    mov , %rax #syscall: sys_write

--- hello.s
+++ hello.s
@@ -6,6 +6,12 @@ hello:
 .globl _start #.global valid too
 
 _start:	
-    mov $60, %rax #syscall: sys_exit
-    xor %rdi, %rdi #exit status: 0, rdi = register destination index
+    mov $1, %rax #syscall: sys_write, rax = Register Accumulator eXtended
+    mov $1, %rdi #stdout, rdi = register destination index
+    mov $hello, %rsi #char * address
+    mov $10, %rdx #string length, rdx = Register Data eXtended
     syscall #calls the kernel
+
+    mov $60, %rax #syscall: sys_exit
+    xor %rdi, %rdi #exit status: 0
+    syscall
