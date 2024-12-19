fb5bb952495863b788c5a8cbec19b5c0dd430949
nengkya <nengkya@yahoo.com>
2024-12-19 08:46:00 +0700

    mov al, (%rdi) #move al into place pointed by rdi

--- copy.s
+++ copy.s
@@ -2,12 +2,27 @@
     .lcomm buffer, 13	#local  common area/allocated for two files refer to different memory location in bss.
     .comm  buffes, 13	#global common area/allocated for two files refer to same      memory location in bss.
 
+.data
+hello:
+    .ascii "Hello\n\0"
 
 
 .text
 .globl _start
 
 _start:
-    mov $60, %rax  	#exit
+    mov $hello,  %rsi	#char * src
+    mov $buffer, %rdi   #char * dest
+    call copy_string
+
+    mov $60,  %rax  	#exit
     xor %rdi, %rdi 	#0
     syscall	  	#call kernel
+
+copy_string:
+    mov $13, %rcx	#loop count
+.Loop:
+    mov (%rsi), al	#move value pointed by rsi into al
+    mov al, (%rdi)	#move al into place pointed by rdi
+    inc %rsi
+    inc %rdi
